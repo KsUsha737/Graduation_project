480b74e387feef563c64b73e55f36995585869c9
anle75 <anle751@gmail.com>
2022-08-24 19:44:56 +0300

    fix all

--- src/test/java/API/apiClient/LeadClientApi.java
+++ src/test/java/API/apiClient/LeadClientApi.java
@@ -2,38 +2,44 @@ package API.apiClient;
 
 import API.base.BaseApi;
 import API.dto.*;
+import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import utils.PropertiesKeys;
 import utils.PropertyReader;
 
 public class LeadClientApi extends BaseApi {
-
-    public String createLead(Lead lead){
-        Response response = post(gson.toJson(lead), PropertyReader.getProperty(PropertiesKeys.API_SOBJ_URL_LEAD),201);
+    @Step("create Lead")
+    public String createLead(Lead lead) {
+        Response response = post(gson.toJson(lead), PropertyReader.getProperty(PropertiesKeys.API_SOBJ_URL_LEAD), 201);
         System.out.println(response.body().asString());
         return gson.fromJson(response.body().asString(), CreatedLeadResponse.class).getId();
     }
 
-    public LeadResponse getById(String id){
-        Response response = get(PropertyReader.getProperty(PropertiesKeys.API_SOBJ_URL_LEAD)+id,200);
-        return gson.fromJson(response.body().asString(),LeadResponse.class);
+    @Step("get Lead by id")
+    public LeadResponse getById(String id) {
+        Response response = get(PropertyReader.getProperty(PropertiesKeys.API_SOBJ_URL_LEAD) + id, 200);
+        return gson.fromJson(response.body().asString(), LeadResponse.class);
     }
 
-    public int deleteLead(String id){
-        return delete(PropertyReader.getProperty(PropertiesKeys.API_SOBJ_URL_LEAD)+id,204).getStatusCode();
+    @Step("delete Lead by id")
+    public int deleteLead(String id) {
+        return delete(PropertyReader.getProperty(PropertiesKeys.API_SOBJ_URL_LEAD) + id, 204).getStatusCode();
     }
 
-    public int updateLeadById(Lead fieldsToUpdate,String leadId){
-        return path(gson.toJson(fieldsToUpdate),PropertyReader.getProperty(PropertiesKeys.API_SOBJ_URL_LEAD)+leadId,204).getStatusCode();
+    @Step("update Lead")
+    public int updateLeadById(Lead fieldsToUpdate, String leadId) {
+        return path(gson.toJson(fieldsToUpdate), PropertyReader.getProperty(PropertiesKeys.API_SOBJ_URL_LEAD) + leadId, 204).getStatusCode();
     }
 
-    public LeadsList getAllLeads(){
+    @Step("get all Leads")
+    public LeadsList getAllLeads() {
         Response response = get(PropertyReader.getProperty(PropertiesKeys.API_SOBJ_URL_LEAD), 200);
         return gson.fromJson(response.body().asString(), LeadsList.class);
     }
 
-    public void deleteAllLeads(){
-        for(LeadInfo leadInfo:getAllLeads().getLeadInfoList()){
+    @Step("delete all Leads")
+    public void deleteAllLeads() {
+        for (LeadInfo leadInfo : getAllLeads().getLeadInfoList()) {
             deleteLead(leadInfo.getId());
         }
     }
